cb83750c4089952dec9356c23f2d252173107445
Namima Islam <namimaislam308@gmail.com>
2024-07-05 16:36:37 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//capitalize the first letter of each word
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -12,5 +14,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  });
+};
+
+console.log(titleCased());
