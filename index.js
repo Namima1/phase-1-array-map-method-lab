cb1a60170a6151bfeb4d9a9fb9b4e835473eb667
Namima Islam <namimaislam308@gmail.com>
2024-09-25 12:44:47 -0400

    lab

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-//capitalize the first letter of each word
+//Your job is to write the following function:
+
+//titleCased(): returns an array with title case tutorial names. Note that this function takes no arguments and should use the global tutorials variable as data.
+
+//Some questions to consider:
+
+// How can we "iterate" through individual words in a string?- split
+// Can we execute an iteration inside an iteration? How?, loop, for..of
+// How can we capitalize just the first letter in a word?- toUpperCase
+// A couple of hints:
+
+// Break the task into smaller chunks: using the console or a REPL, start by figuring out how to modify one individual element in the tutorials array. Once you've got that working, then figure out how to update the array itself.
+
+//task: We want all the titles to be "title case", in other words, the first letter of each word should be capitalized. Create a new array containing the names of the tutorials with proper title case formatting. For example, 'what does the this keyword mean?' should become 'What Does The This Keyword Mean?'.
+
+//split all the sentences, use to uppercase to format
 
 const tutorials = [
   'what does the this keyword mean?',
@@ -13,10 +28,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials.map(tutorial => {
-    return tutorial.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-  });
-};
+ const titleCased = () => tutorials.map((tutorial) => {
+ //  return tutorial.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+const stringArray = tutorial.split(' ')
+const arrayCapitalize = stringArray.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+return arrayCapitalize.join(' ') 
+
+ });
+
+
+
+
+ 
 
 console.log(titleCased());
+
